01c401b3342fe7587c4649e515e98594d93057b2
Agnieszka <106694089+agkittens@users.noreply.github.com>
2024-02-11 18:36:57 +0100

    Add files via upload

--- interface/app.py
+++ interface/app.py
@@ -13,47 +13,82 @@ if "start" not in st.session_state:
     st.session_state.start = False
 
 if "text" not in st.session_state:
-    st.session_state.text = 4
+    st.session_state.text = 21
 
+if "lvl" not in st.session_state:
+    st.session_state.lvl = 1
+
+if "research" not in st.session_state:
+    st.session_state.research = False
 
 
 def start():
     with st.container(border=True,height=600):
         img, start_b, lvls_b = st.columns([1, 0.9, 1])
 
-        #nie wiem czemu tak ma byc ale wtedy dziala
+        #nie wiem czemu tak ma byc ale wtedy dziala znikanie po wcisnieciu start
         if not st.session_state.start:
             st.session_state.start = True
             if start_b.button("Start", type="secondary", use_container_width=True):
                 pass
 
         elif st.session_state.start:
+
             utilis.margin_top(40)
             img.image("assets/scientist.png", width=350)
             utilis.margin_top(30)
+            print(st.session_state.research)
+
             messages = st.container(height=150, border=True)
-            messages.write_stream(utilis.stream_data(st.session_state.text))
-            st.session_state.text += 1
 
-            if st.session_state.text > 5:
+
+            #introduction
+            if st.session_state.text < 8:
+                messages.write_stream(utilis.stream_data(st.session_state.text))
+
+            #a little bit about mbti types
+            elif st.session_state.text >= 8:
+                messages.write_stream(utilis.stream_data2(st.session_state.text-8))
+
+            # if not st.session_state.research:
+            if st.session_state.text <24:
+                st.session_state.text+=1
+
+            if st.session_state.text == 6 and st.session_state.lvl == 0:
+                st.session_state.text = 5
+
+                #TUTAJ SA POZIOMY
                 if lvls_b.button("Easy", type="secondary", use_container_width=True):
-                    pass
+                    st.session_state.lvl = 1
+                    st.session_state.text += 1
+
                 elif lvls_b.button("Medium", type="secondary", use_container_width=True):
-                    pass
+                    st.session_state.lvl = 2
+                    st.session_state.text += 1
+
                 elif lvls_b.button("Hard", type="secondary", use_container_width=True):
-                    pass
+                    st.session_state.lvl = 3
+                    st.session_state.text += 1
+                #TU KONIEC POZIOMOW
 
-            if messages.button("→", type="secondary"):
+
+            if st.session_state.text >=24:
+                st.session_state.text = 23
                 messages.empty()
+                if messages.button("Start research", type="secondary"):
+                    st.session_state.research = True
+                    st.rerun()
 
-
+            elif st.session_state.text <24:
+                if messages.button("→", type="secondary") or st.session_state.lvl > 0:
+                    messages.empty()
 
 
 
 
 def manage():
-    if st.session_state.window == "game": game.game()
-    elif st.session_state.window == "help": help.help()
+    if st.session_state.window == "game" or st.session_state.research: game.game()
+    elif st.session_state.window == "help" : help.help()
     elif st.session_state.window == "start": start()
 
 with st.sidebar:
--- interface/utilis.py
+++ interface/utilis.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import time
+import pandas as pd
 
 def add_logo():
     st.markdown(
@@ -43,10 +44,23 @@ def stream_data(idx):
         "Your task is to delve into the mysteries of human personality, all within the framework of exercises in the laboratory. After greeting your mentor, you head deeper into the laboratory, ready for the next stage of your education. Your skills will be put to the test during a trial where you must match the personality type to each of the robots. By talking to them and asking questions, you must explore their nature to later create credible copies of people.",
         "An extraordinary journey begins through the intricacies of the human psyche and technological manipulation, where your decisions will shape the future of this cyberpunk world of science. Are you ready for the challenge? Take the test and delve into the secrets of the human soul to become a master of the art of creating perfect replicas of human personality.",
         "During your research you can use my notes. They are on the left panel of your tablet.",
-        "So let's get started. Considering that today is your first day of testing, I'll let you choose the set for testing. I have prepared three sets of robots for you: Small set contains 4, medium 5, and large 6."
+        "So let's get started. Considering that today is your first day of testing, I'll let you choose the set for testing. I have prepared three sets of robots for you: Small set contains 4, medium 5, and large 6.",
+        "Great choice. Firstly, I am going to explain to you basic concepts of each of MBTI type and then you can start your resarch. I will be waiting for the results till 8 PM. Do not disappoint me.",
+        "All types are unique, there are 16 of them."
     ]
     for word in texts[idx].split():
         yield word + " "
         time.sleep(0.07)
 
+def stream_data2(idx):
+    mbti_data = pd.read_json("mbti_data.json")
+    mbti_data = pd.DataFrame(mbti_data)
+
+    for word in mbti_data.iloc[idx]['type'].split():
+        yield word + " "
+        time.sleep(0.07)
+
+    for word in mbti_data.iloc[idx]['description'].split():
+        yield word + " "
+        time.sleep(0.07)
 
