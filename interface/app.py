e6137b80bf68b8c3ba4df29697a696e7df000a00
Marlon1385 <marlon1385@interia.pl>
2024-06-25 12:56:00 +0200

    powinno działać

--- interface/app.py
+++ interface/app.py
@@ -1,9 +1,15 @@
 import streamlit as st
 import utilis, game, help, about, streak
+import firebase_admin
+from firebase_admin import db, credentials
+import pandas as pd
+
 
 st.set_page_config(page_title="Who am I?", layout="wide", initial_sidebar_state="collapsed")
 utilis.add_logo()
 utilis.remove_space()
+
+#Session state data initialized on app start
 
 if 'username' not in st.session_state:
     st.session_state.username = ""
@@ -29,8 +35,82 @@ if "level" not in st.session_state:
 if "gamemode" not in st.session_state:
     st.session_state.gamemode = None
 
-def start():
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
 
+if 'data' not in st.session_state:
+    st.session_state.data = pd.DataFrame({'username': [], 'score': []})
+
+if 'data_streak' not in st.session_state:
+    st.session_state.data_streak = pd.DataFrame({'username': [], 'score': []})
+
+if 'score' not in st.session_state:
+    st.session_state.score = 0
+
+if 'streak_classic' not in st.session_state:
+    st.session_state.streak_classic = 0
+
+if 'curr_lvl' not in st.session_state:
+    st.session_state.curr_lvl = 1
+
+if 'hints_classic' not in st.session_state:
+    st.session_state.hints_classic = 3
+
+if 'hints_streak' not in st.session_state:
+    st.session_state.hints_streak = 3
+
+if 'score2' not in st.session_state:
+    st.session_state.score2 = 0
+
+if 'streak_streak' not in st.session_state:
+    st.session_state.streak_streak = 0
+
+if 'current_lvl' not in st.session_state:
+    st.session_state.current_lvl = 1
+
+if 'tries' not in st.session_state:
+    st.session_state.tries = 10
+
+if 'guesses2' not in st.session_state:
+    st.session_state.guesses2 = 1
+
+if 'botsStreakList' not in st.session_state:
+    data = pd.read_json("data.json")
+    data = pd.DataFrame(data)
+    st.session_state.botsStreakList = data
+
+if 'allMbti' not in st.session_state:
+    data = pd.read_json("data.json")
+    data = pd.DataFrame(data)
+    st.session_state.allMbti = data['mbti'].tolist()
+
+if 'tutorial_classic' not in st.session_state:
+    st.session_state.tutorial_classic = False
+
+if 'tutorial_streak' not in st.session_state:
+    st.session_state.tutorial_streak = False
+def login():
+
+    st.write("### LOG IN")
+    username = st.text_input("Username:")
+    password = st.text_input("Password:", type="password")
+
+    if st.button("LOG IN"):
+        us = db.reference(f"/{username}").get()
+        if us == None:
+            db.reference("/").update({f"{username}/password": f"{password}"})
+            db.reference("/").update({f"{username}/score_classic": "0"})
+            db.reference("/").update({f"{username}/score_streak": "0"})
+            st.error("Created an account, log in again")
+        else:
+            pas = db.reference(f"/{username}/password").get()
+            if pas == password:
+                st.session_state.logged_in = True
+                st.session_state.username = username
+                st.experimental_rerun()  # Re-run the app to proceed to the main content
+            else:
+                st.error("A user with this name exists, but you entered the wrong password")
+def start():
     with st.container(border=True,height=600):
         img, start_b, lvls_b = st.columns([1, 0.9, 1])
 
@@ -114,15 +194,32 @@ def start():
 
 
 def manage():
-    if st.session_state.window == "game":
+    if st.session_state.window == "game" and not st.session_state.tutorial_classic:
+        st.session_state.gamemode = 0
+        game.tutorial()
+    elif st.session_state.window == "game" and st.session_state.tutorial_classic:
         st.session_state.gamemode = 0
         game.game()
     elif st.session_state.window == "help" : help.help()
     elif st.session_state.window == "start": start()
     elif st.session_state.window == "about": about.about()
-    elif st.session_state.window == "streak":
+    elif st.session_state.window == "streak" and not st.session_state.tutorial_streak:
+        st.session_state.gamemode = 1
+        streak.tutorial()
+    elif st.session_state.window == "streak" and st.session_state.tutorial_streak:
         st.session_state.gamemode = 1
         streak.streak()
+
+if not st.session_state.logged_in:
+    if not firebase_admin._apps:
+        cred_obj = firebase_admin.credentials.Certificate('cred.json')
+        default_app = firebase_admin.initialize_app(cred_obj, {
+            'databaseURL': "https://who-am-i-c969c-default-rtdb.europe-west1.firebasedatabase.app/"})
+    ref = db.reference("/")
+    login()
+else:
+    if st.session_state.window!="game" and st.session_state.window!="streak":
+        st.session_state.window = "start"
 
 with st.sidebar:
     utilis.margin_top(40)
@@ -130,22 +227,21 @@ with st.sidebar:
     utilis.margin_bottom(30)
 
     b0 = st.button("Classic Game", type="secondary", use_container_width=True)
-    b5 = st.button("Streak challenge", type="secondary", use_container_width=True)
+    b4 = st.button("Streak challenge", type="secondary", use_container_width=True)
     b1 = st.button("Get help from Professor", type="secondary", use_container_width=True)
     b2 = st.button("Start again your research", type="secondary", use_container_width=True)
-    b3 = st.button("Settings", type="secondary", use_container_width=True)
-    b4 = st.button("About us", type="secondary", use_container_width=True)
+    b3 = st.button("About us", type="secondary", use_container_width=True)
 
 
-
-if b0:
-    st.session_state.window = "game"
-elif b1:
-    st.session_state.window = "help"
-elif b2:
-    st.session_state.window = "start"
-elif b4:
-    st.session_state.window = "about"
-elif b5:
-    st.session_state.window = "streak"
+if st.session_state.logged_in:
+    if b0:
+        st.session_state.window = "game"
+    elif b1:
+        st.session_state.window = "help"
+    elif b2:
+        st.session_state.window = "start"
+    elif b3:
+        st.session_state.window = "about"
+    elif b4:
+        st.session_state.window = "streak"
 manage()
--- interface/streak.py
+++ interface/streak.py
@@ -3,40 +3,31 @@ import pandas as pd
 from streamlit_modal import Modal
 import random
 from ChatManager.ChatManager import CM
+from firebase_admin import db, credentials
+import utilis
 
+def tutorial():
+    # Custom CSS to center the title
+    st.markdown(
+        """
+        <style>
+        .centered-title {
+            text-align: center;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
-#DATA
-if 'data' not in st.session_state:
-    st.session_state.data = pd.DataFrame({'username': [], 'score': []})
-
-if 'hints' not in st.session_state:
-    st.session_state.hints = 3
-
-if 'score2' not in st.session_state:
-    st.session_state.score2 = 0
-
-if 'streak' not in st.session_state:
-    st.session_state.streak = 0
-
-if 'current_lvl' not in st.session_state:
-    st.session_state.current_lvl = 1
-
-if 'tries' not in st.session_state:
-    st.session_state.tries = 10
-
-if 'guesses2' not in st.session_state:
-    st.session_state.guesses2 = 1
-
-if 'botsStreakList' not in st.session_state:
-    data = pd.read_json("data.json")
-    data = pd.DataFrame(data)
-    st.session_state.botsStreakList = data
-
-if 'allMbti' not in st.session_state:
-    data = pd.read_json("data.json")
-    data = pd.DataFrame(data)
-    st.session_state.allMbti = data['mbti'].tolist()
-
+    utilis.remove_space()
+    with st.container(border=True):
+        # Use the custom CSS class for centering
+        st.markdown("<h1 class='centered-title'>How to play Streak Challenge?</h1>", unsafe_allow_html=True)
+        st.write("siema")
+        if st.button("Let's Play", type="primary", use_container_width=True):
+            st.session_state.window = "streak"
+            st.session_state.tutorial_streak = True
+            st.rerun()
 
 def streak():
 
@@ -49,7 +40,6 @@ def streak():
         return update
 
 
-    # @st.cache_data
     def get_data():
         botsStreakList = st.session_state.botsStreakList
         random_mbti = random.sample(st.session_state.allMbti, 1 + additional_personalities())
@@ -59,18 +49,8 @@ def streak():
         index = namesStreak.index(chosenBotName)
         chosenBotMbti = mbtiStreak[index]
 
-        print("TEST 0")
-        print(chosenBotName)
-        print(chosenBotMbti)
-
         st.session_state.botsStreakList = botsStreakList[
             botsStreakList['name'] != chosenBotName]
-
-        print("TEST")
-        print(random_mbti)
-        print(chosenBotName)
-        print(chosenBotMbti)
-        print("pitu pitu")
 
         return chosenBotName, chosenBotMbti, random_mbti
 
@@ -89,28 +69,25 @@ def streak():
     def shuffle(mbtis):
         return random.sample(mbtis, len(mbtis))
 
-
-    def update_scoreboard(score, df, user = st.session_state.username):
+    def update_scoreboard(score, df, user=st.session_state.username):
         new_data = {'username': [user], 'score': [score]}
         data = pd.DataFrame(new_data)
-        update = pd.concat([df,data],ignore_index=True)
+        update = pd.concat([df, data], ignore_index=True)
+        db.reference(f"/{user}/score_streak").set(str(st.session_state.score2))
         return update
 
-    def modified_stream_generator(original_stream):
-        original_list = list(original_stream)
-        num_chars_to_replace = int(0.1 * len(original_list))
-        indexes_to_replace = random.sample(range(len(original_list)), num_chars_to_replace)
-        for i, char in enumerate(original_list):
-            if i in indexes_to_replace:
-                yield '*'
-            else:
-                yield char
-
+    def get_all_scores():
+        users = db.reference("/").get()
+        data = []
+        for user, info in users.items():
+            if "score_streak" in info:
+                data.append({'username': user, 'score': float(info['score_streak'])})
+        return pd.DataFrame(data)
 
     def rerun_level():
         del st.session_state['selected_option_streak']
         st.session_state.current_lvl+=1
-        st.session_state.streak += 1
+        st.session_state.streak_streak += 1
         score = (st.session_state.tries + 1) * 10
         st.session_state.score2 += score
         st.empty()
@@ -128,7 +105,7 @@ def streak():
     def reset_level():
         del st.session_state['selected_option_streak']
         st.session_state.current_lvl=1
-        st.session_state.streak = 0
+        st.session_state.streak_streak = 0
         st.session_state.score2 = 0
         st.session_state.score = 0
         st.empty()
@@ -136,7 +113,7 @@ def streak():
         chosenBot, chosenBotMbti, mbtis = st.session_state.lvl_data_streak
         cm.reset(BOTS)
         cm.add_defaulted_system_prompt(chosenBot)
-        st.session_state.hints = 3
+        st.session_state._streak = 3
         if st.session_state.current_lvl <= 25:
             st.session_state.tries = 10 - (int(st.session_state.current_lvl/5))
         else:
@@ -145,33 +122,25 @@ def streak():
 
     def gameOverInfo():
         gameover = Modal(key="gameover", title="Game Over")
+        recent = db.reference(f"/{st.session_state.username}/score_streak").get()
+        if float(recent) < st.session_state.score2:
+            db.reference("/").update({f"{st.session_state.username}/score_streak": f"{st.session_state.score2}"})
 
         with gameover.container():
             if st.session_state.tries == 0:
                 st.write("You have exceeded the number of available tries!")
             else:
                 st.write("Your answer was incorrect!")
+                st.write(f"Your score in this game was: {st.session_state.score2}")
 
-            temp1, temp2, temp3, temp4 = st.columns([0.3, 1, 0.3,1], gap='small')
+            all_scores = get_all_scores()
+            sorted_scores = all_scores.sort_values(by='score', ascending=False)
+            st.dataframe(sorted_scores, use_container_width=True)
 
-            with temp2:
-                if st.button("Try Again", type="primary", on_click=reset_level): pass
+            temp1, temp2, temp3, temp4, temp5 = st.columns([0.3, 1, 1,0.3,0.3], gap='small')
 
-            with temp4:
-                if st.button("Show Scoreboard", type="primary"):
-                    scoreboard = Modal(key="score", title="Scoreboard")
-
-                    with scoreboard.container():
-                        sorted = st.session_state.data.sort_values(by='score', ascending=False)
-                        st.dataframe(sorted, use_container_width=True)
-
-                        temp1, temp2 = st.columns([0.3, 1], gap='small')
-
-                        # w funkcji rerun level jest reset ustawien i trzeba dodac zarzadzanie levelami
-                        with temp1:
-                            if st.button("Try Again", type="primary", on_click=reset_level): pass
-
-
+            with temp3:
+                if st.button("Try Again", type="primary", on_click=reset_level):pass
 
     BOTS = 1
 
@@ -181,26 +150,15 @@ def streak():
     if 'lvl_data_streak' not in st.session_state:
         st.session_state.lvl_data_streak = get_data()
     chosenBot, chosenBotMbti, mbtis = st.session_state.lvl_data_streak
-    print("TEST 2")
-    print(chosenBot)
-    print(len(st.session_state.botsStreakList))
 
-    print("Test 2.5")
+    print("Poprawna Odpowiedź")
     print(chosenBotMbti)
-    print(mbtis)
     isthere = all(element != chosenBotMbti for element in mbtis)
     if isthere:
         mbtis.pop()
         mbtis.append(chosenBotMbti)
 
-    print("TEST 3")
-    print(isthere)
-    print(mbtis)
-
     shuffled_toguess = shuffle(mbtis)
-
-    print("TEST 4")
-    print(shuffled_toguess)
 
     #containery
     chat, bots, guesses = st.columns([1.5,1, 0.8], gap="medium")
@@ -213,9 +171,8 @@ def streak():
         temp1, temp2, temp3, temp4, temp5= st.columns([1,1,1,1,1], gap="small")
         with temp1: st.write(f"name: {st.session_state.username}")
         with temp2: st.write(f"score: {st.session_state.score2}")
-        with temp3: st.write(f"streak: {st.session_state.streak}")
+        with temp3: st.write(f"streak: {st.session_state.streak_streak}")
         with temp5: st.write(f"tries: {st.session_state.tries}")
-        print("Sprawdzam ten shiet")
 
         #chats
         with st.container(border=True):
@@ -235,32 +192,29 @@ def streak():
                 messages.chat_message("user").write(prompt)
                 cm.add_user_message(prompt)
                 stream = cm.get_response_stream()
-                #if st.session_state.current_lvl>2:
-                #    stream=modified_stream_generator(stream)
                 with messages.chat_message("assistant"):
                     st.write_stream(stream)
                 st.session_state.tries -= 1
                 st.rerun()
 
-
     with bots:
         #avaible hints above bots img
-        st.write(f"hints: {st.session_state.hints}")
+        st.write(f"hints: {st.session_state.hints_streak}")
         with st.container(border=True, height=550):
             st.image("assets/temp.png", use_column_width=True)
 
 
     with guesses:
         #hints button and select boxes
-        if st.button("Call Professor", type="primary", use_container_width=True) and st.session_state.hints > 0:
+        if st.button("Call Professor", type="primary", use_container_width=True) and st.session_state.hints_streak > 0:
             help = Modal(key="help",title="Help")
-            st.session_state.hints-=1
+            st.session_state.hints_streak-=1
             with help.container():
                 img, text = st.columns([0.8,1.2], gap="large")
                 with img: st.image("assets/scientist.png", use_column_width=True)
                 with text:
                     #tutaj prmpty podpowiedzi dla wybranego bota
-                    st.write(f"selected bot: {cm.selected_p+1}")
+                    #st.write(f"selected bot: {cm.selected_p+1}")
                     st.write(cm.get_hint_stream())
 
 
@@ -278,27 +232,17 @@ def streak():
             if 'selected_option_streak' not in st.session_state:
                 temp = [None]
                 st.session_state.selected_option_streak = temp
-
-            #if not option is None:
-             #   st.session_state.selected_option[0] = shuffled_toguess.index(option)
             options.append(option)
-
-
-
-
         #finish game
         if st.button("Next round", type="secondary", use_container_width=True):
             rightPick = False
             if options[0] == chosenBotMbti:
                 rightPick = True
-
             #score and streak managment
-
-
             #streak
             if rightPick == True:
                 print("Guessed correcly")
                 rerun_level()
-
+            #Wrong Answer
             else:
                 gameOverInfo()
